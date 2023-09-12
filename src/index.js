84dd73ad6e85c6a939e189433a87b072aeb42d9d
joey-ammar <xyx8703@gmail.com>
2023-09-12 08:25:27 +0200

    added some prop stuff

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ const books = [
 ];
 
 function BookList() {
+  const displayFunction = () => {
+    console.log("Hey");
+  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        let { id } = book.id;
         return (
-          <div>
-            <Book {...book} key={id} />
-          </div>
+          <Book {...book} key={book.id} displayFunction={displayFunction} />
         );
       })}
     </section>
@@ -35,11 +35,15 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
+  const displayTitle = () => {
+    console.log(title);
+  };
 
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>display title</button>
       <h4>{author} </h4>
     </article>
   );
