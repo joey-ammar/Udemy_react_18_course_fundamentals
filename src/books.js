5c226ca332f502c17760e02ae3b8a8f25d7326a1
joey-ammar <xyx8703@gmail.com>
2023-09-14 10:32:44 +0200

    finished unit one

---
+++ src/books.js
@@ -0,0 +1,29 @@
+/**First Book */
+
+import img1 from "./images/img1.jpeg";
+import img2 from "./images/img1.jpeg";
+import img3 from "./images/img1.jpeg";
+
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 1,
+    img: img1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+    img: img2,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+    img: img3,
+  },
+];
